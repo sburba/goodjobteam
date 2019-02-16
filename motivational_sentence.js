f3b77b19d05c2827f8737e1ea62daf7862a16737
Sam Burba <sburba@barracuda.com>
2019-02-16 09:00:34 -0500

    Initial commit

---
+++ motivational_sentence.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const goodSynonyms = [
+		'Good',
+		'Excellent',
+		'Exceptional',
+		'Favorable',
+		'Extraordinary',
+		'Great',
+		'Marvelous',
+		'Positive',
+		'Satisfying',
+		'Superb',
+		'Mind-blowing'
+	];
+	const workSynonyms = [
+		'job',
+		'work',
+		'operation',
+		'effort',
+		'endeavor',
+		'performance',
+		'task',
+	];
+	const teamSynonyms = [
+		'team',
+		'comrades',
+		'squad',
+		'crew',
+		'gang',
+		'clan',
+		'troop',
+		'assemblage',
+		'possy',
+		'troupe',
+	];
+
+	function choose(wordList) {
+		return wordList[Math.floor(Math.random() * wordList.length)]
+	}
+
+	let motivationalSentence = `${choose(goodSynonyms)} ${choose(workSynonyms)}, ${choose(teamSynonyms)}!`;
+	document.getElementById('motivational-sentence').textContent = motivationalSentence;
+	document.title = motivationalSentence;
+});
