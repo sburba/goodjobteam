4c1d0956ad7d845a4d17495693c083e0b7bd363d
Sam Souder <samsouder@gmail.com>
2019-02-20 20:36:37 -0600

    Split up synonyms into separate files

--- src/motivational_sentence.js
+++ src/motivational_sentence.js
@@ -1,50 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const goodSynonyms = [
-    "Good",
-    "Excellent",
-    "Exceptional",
-    "Favorable",
-    "Extraordinary",
-    "Great",
-    "Marvelous",
-    "Satisfying",
-    "Superb",
-    "Mind-blowing",
-    "Wonderous",
-    "Stupendous"
-  ];
-  const workSynonyms = [
-    "job",
-    "work",
-    "operation",
-    "effort",
-    "endeavor",
-    "performance",
-    "task",
-    "undertaking",
-    "enterprise",
-    "venture"
-  ];
-  const teamSynonyms = [
-    "team",
-    "comrades",
-    "squad",
-    "crew",
-    "gang",
-    "clan",
-    "assemblage",
-    "posse",
-    "troupe",
-    "amigos"
-  ];
+import goodSynonyms from "./synonyms/good.js";
+import teamSynonyms from "./synonyms/team.js";
+import workSynonyms from "./synonyms/work.js";
 
+document.addEventListener("DOMContentLoaded", () => {
   function choose(wordList) {
     return wordList[Math.floor(Math.random() * wordList.length)];
   }
 
-  let motivationalSentence = `${choose(goodSynonyms)} ${choose(
-    workSynonyms
-  )}, ${choose(teamSynonyms)}!`;
+  function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  const motivationalSentence = `${capitalizeFirstLetter(
+    choose(goodSynonyms)
+  )} ${choose(workSynonyms)}, ${capitalizeFirstLetter(choose(teamSynonyms))}!`;
   document.getElementById(
     "motivational-sentence"
   ).textContent = motivationalSentence;
