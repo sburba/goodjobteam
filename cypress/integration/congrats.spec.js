5b6ba9012a887c97106f5e00795fbb1fa2ec7874
Sam Burba <gitlab@samburba.com>
2019-06-23 16:17:16 -0400

    Add a test for the another button

--- cypress/integration/congrats.spec.js
+++ cypress/integration/congrats.spec.js
@@ -3,6 +3,26 @@ context("Actions", () => {
     cy.visit("/");
   });
 
+  it('Should pick a sentence', () => {
+    cy.get("#container").contains(/\w+ \w+,.+\w+!/ms);
+  });
+
+  it('Gives a new sentence when "another?" is clicked', () => {
+    cy.get('#container').then(($container) => {
+      const firstSentence = $container.text();
+      expect(firstSentence).to.match(/\w+ \w+,.+\w+!/ms);
+
+      cy.get('#new-sentence').click();
+
+      cy.get('#container').should(($container) => {
+        const secondSentence = $container.text();
+        expect(secondSentence).to.match(/\w+ \w+,.+\w+!/ms);
+
+        expect(firstSentence).not.to.equal(secondSentence);
+      })
+    })
+  });
+
   it("Turns to dogs when the konami code is entered", () => {
     const now = new Date();
     // On April first, konami will be there right away
@@ -26,9 +46,5 @@ context("Actions", () => {
     ]);
 
     cy.get("#sentence-icon").should("have.class", "konami");
-  });
-
-  it("Should pick a sentence", () => {
-    cy.get("#container").contains(/\w+ \w+,.+\w+!/ms);
   });
 });
